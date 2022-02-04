c0ba2187298fa4a2bbda8007f99e8dfedfef9aa7
Babailan <ronelbabailan@hotmail.com>
2022-02-04 18:44:37 +0800

    updatedDayOfProgrammer

--- compe/day-of-programmer.js
+++ compe/day-of-programmer.js
@@ -1,8 +1,16 @@
 function dayOfProgrammer(year) {
+  // Write your code here
   if (year === 1918) {
     return `26.09.${year}`;
   }
+  if (year > 1918) {
+    //check if leap
+    const isThisLeapYear =
+      (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+    return `${isThisLeapYear ? "12" : "13"}.09.${year}`;
+  }
+  if (year < 1918) {
+    //
+    return `${year % 4 === 0 ? "12" : "13"}.09.${year}`;
+  }
 }
-
-// expected day 26.09.1918
-dayOfProgrammer(1918);
